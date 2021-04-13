691adf92cc142a7d2922c874b155ec4d75fd6383
taka- <taka-@192.168.1.49>
2021-04-13 17:11:21 +0900

    first commit

---
+++ src/main/java/sample/IndexController.java
@@ -0,0 +1,60 @@
+package sample;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class ServletController
+ */
+@WebServlet("/IndexController")
+public class IndexController extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public IndexController() {
+        super();
+
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		
+		
+		 getServletConfig().getServletContext().
+	      getRequestDispatcher("/new.jsp").forward(request, response);
+	  
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+		String address="/default";//遷移先のアドレス
+		request.setCharacterEncoding("UTF-8");
+		String buttonName=request.getParameter("buttonName");
+		
+		switch(buttonName) {
+		case "newThread":
+			address="/new.jsp";
+			break;
+		case "search":
+			address="/new.jsp";
+			break;
+		}
+		
+		 getServletConfig().getServletContext().
+	      getRequestDispatcher(address).forward(request, response);
+	  
+	}
+
+}
---
+++ src/main/webapp/footer.jsp
@@ -0,0 +1,14 @@
+<%@ page language="java" contentType="text/html; charset=UTF-8"
+    pageEncoding="UTF-8"%>
+<footer class="text-muted">
+		<div class="container">
+			<div class="float-end">
+				<a href="#">ページトップ</a>
+			
+			<div class="float-right">作成者 川内</div>
+			
+				<a href="ServletController">ホームへ戻る</a>
+			
+			</div>
+		</div>
+	</footer>
