6880dfc7ac47f397ded3808908a60d30a0eeebe7
taka- <taka-@192.168.1.49>
2021-04-14 15:28:58 +0900

    threadbean.javaの作成

--- src/main/java/sample/ThreadBean.java
+++ src/main/java/sample/ThreadBean.java
@@ -1,5 +1,14 @@
 package sample;
 
 public class ThreadBean {
-
+ private int id;
+ private String title;
+ private String name;
+ private String message;
+ 
+ ThreadBean(String title,String name,String message){
+	 this.title=title;
+	 this.name=name;
+	 this.message=message;
+ }
 }
