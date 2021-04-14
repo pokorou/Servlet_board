8314960679d129eb02fb0f81391c8b80c171ca58
taka- <taka-@192.168.1.49>
2021-04-14 15:27:35 +0900

    ConnectionDBへの処理を追加

--- src/main/java/sample/IndexController.java
+++ src/main/java/sample/IndexController.java
@@ -13,50 +13,41 @@ import javax.servlet.http.HttpServletResponse;
  */
 @WebServlet("/IndexController")
 public class IndexController extends HttpServlet {
+	String name="aaa";
 	private static final long serialVersionUID = 1L;      
     public IndexController() {
         super();
+        
     }
 
-	/**
-	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
-	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		
-		
-		
-		
-		
-		
-		
-		
-		
 		 getServletConfig().getServletContext().
-	      getRequestDispatcher("/new.jsp").forward(request, response);
-	  
+	      getRequestDispatcher("/index.jsp").forward(request, response);
 	}
 
-	/**
-	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
-	 */
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-
+		request.setCharacterEncoding("UTF-8");
+		ConnectionDB connectionDB=new ConnectionDB();
 		String address="/default";//遷移先のアドレス
 		request.setCharacterEncoding("UTF-8");
 		String buttonName=request.getParameter("buttonName");
 		
 		switch(buttonName) {
 		case "newThread":
+			System.out.println("パラメータ（title）"+request.getParameter("title"));
+			connectionDB.newThread(request.getParameter("title"),name, request.getParameter("message"));
+			request.setAttribute("data",  connectionDB.selectAllThread());		
 			address="/index.jsp";
 			break;
 		case "search":
 			address="/index.jsp";
 			break;
-		}
-		
+		case"commentView":
+			request.setAttribute("data",  connectionDB.selectAllComment());		
+			address="/commentView.jsp";
+		}	
 		 getServletConfig().getServletContext().
-	      getRequestDispatcher(address).forward(request, response);
-	  
+	      getRequestDispatcher(address).forward(request, response);	  
 	}
 
 }
